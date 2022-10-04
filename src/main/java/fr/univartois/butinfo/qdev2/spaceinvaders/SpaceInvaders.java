2d36f540026234dd4630f5ef4133a71eae91e725
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-10-04 13:56:18 +0200

    add factory attributes

--- src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/SpaceInvaders.java
@@ -19,8 +19,10 @@ package fr.univartois.butinfo.qdev2.spaceinvaders;
 import java.io.IOException;
 
 import fr.univartois.butinfo.qdev2.spaceinvaders.controller.SpaceInvadersController;
+import fr.univartois.butinfo.qdev2.spaceinvaders.model.IMovableFactory;
 import fr.univartois.butinfo.qdev2.spaceinvaders.model.MovableFactory;
 import fr.univartois.butinfo.qdev2.spaceinvaders.model.SpaceInvadersGame;
+import fr.univartois.butinfo.qdev2.spaceinvaders.view.ISpriteStore;
 import fr.univartois.butinfo.qdev2.spaceinvaders.view.SpriteStore;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -60,11 +62,15 @@ public final class SpaceInvaders extends Application {
         Parent viewContent = fxmlLoader.load();
         SpaceInvadersController controller = fxmlLoader.getController();
         controller.setStage(stage);
+        ISpriteStore spriteStore = new SpriteStore();
+        IMovableFactory factory = new MovableFactory();
+        factory.setSpriteStore(spriteStore);
 
         // On crée ensuite le jeu, que l'on lie au contrôleur.
         // TODO Utiliser ici la bonne factory pour créer les objets du jeu.
         SpaceInvadersGame game = new SpaceInvadersGame(
-                GAME_WIDTH, GAME_HEIGHT, new SpriteStore(), new MovableFactory());
+                GAME_WIDTH, GAME_HEIGHT, spriteStore, factory);
+        factory.setGame(game);
         controller.setGame(game);
         game.setController(controller);
         game.prepare();
