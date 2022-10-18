de4cb17a77dbe601b9b85c7adae113ef5bb2a4e1
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-10-18 15:34:54 +0200

    add tir stratégy, non implémenté

---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/ContreAttaqueDefaut.java
@@ -0,0 +1,30 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023.
+ *
+ * (c) 2022 nicolas
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+
+/**
+ * Le type ContreAttaqueDefaut
+ *
+ * @author nicolas
+ *
+ * @version 0.1.0
+ */
+public class ContreAttaqueDefaut implements IContreAttaque {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.IContreAttaque#fireShot()
+     */
+    public void fireShot() {
+        // TODO Auto-generated method stub.
+    }
+
+}
+
---
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/ContreAttaqueRandom.java
@@ -0,0 +1,30 @@
+/**
+ * Ce fichier fait partie du projet projet-2022-2023.
+ *
+ * (c) 2022 nicolas
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.qdev2.spaceinvaders.model.movables;
+
+
+/**
+ * Le type ContreAttaqueRandom
+ *
+ * @author nicolas
+ *
+ * @version 0.1.0
+ */
+public class ContreAttaqueRandom implements IContreAttaque {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.qdev2.spaceinvaders.model.movables.IContreAttaque#fireShot()
+     */
+    public void fireShot() {
+        // TODO Auto-generated method stub.
+    }
+
+}
+
