f8deee446013b28371e03f377f2b23e4bdc5da0c
Thomas Sontag <thomas.sontag@ens.univ-artois.fr>
2022-12-09 10:41:22 +0100

    réglage d'un bug du au merge

--- src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/AbstractMovable.java
+++ src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/AbstractMovable.java
@@ -312,13 +312,11 @@ public abstract class AbstractMovable implements IMovable {
     public Sprite getSprite() {
         return sprite.get();
     }
-<<<<<<< src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/AbstractMovable.java
     
     public void setSprite(Sprite sprite) {
          this.sprite.set(sprite);
     }
-=======
->>>>>>> src/main/java/fr/univartois/butinfo/qdev2/spaceinvaders/model/movables/AbstractMovable.java
+
 
     /*
      * (non-Javadoc)
